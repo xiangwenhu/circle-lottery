73fbfad0ddbfb27421dd27dc1b0b6157d8d86b14
TKOFFICE\huxw <huxw@mail.jj.cn>
2019-06-03 16:45:51 +0800

    修改demo，兼容移动端测试。

--- docs/lottery.js
+++ docs/lottery.js
@@ -1,9 +1,30 @@
-const PRIZE_COUNT = 6;
-let enabledDraw = true;
+function browser() {
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTablet =
+      /(?:iPad|PlayBook)/.test(ua) ||
+      (isAndroid && !/(?:Mobile)/.test(ua)) ||
+      (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+  return {
+    isTablet: isTablet,
+    isPhone: isPhone,
+    isAndroid: isAndroid,
+    isPc: isPc
+  };
+}
 
-const prizesEl = document.querySelector(".draw-btn");
+var PRIZE_COUNT = 6;
+var enabledDraw = true;
 
-let lottery = new Lottery(prizesEl, {
+var prizesEl = document.querySelector(".draw-btn");
+
+var lottery = new Lottery(prizesEl, {
   pits: PRIZE_COUNT,
   angles: [60, 60, 60, 60, 60, 60]
 });
@@ -12,12 +33,15 @@ lottery.onEnded = function(ins, index, prizeIndexes) {
   enabledDraw = true;
 };
 
-document.querySelector(".draw-btn").addEventListener("click", function() {
+var btnDraw = document.querySelector(".draw-btn");
+var eventName = browser().isPc ? "click" : "touchstart";
+
+btnDraw.addEventListener(eventName, function() {
   if (!enabledDraw) {
     return;
   }
   // 设置奖项
-  const indexes = [2];
+  const indexes = [0, 2, 4];
   lottery.setPrize(indexes);
   lottery.start();
 });
