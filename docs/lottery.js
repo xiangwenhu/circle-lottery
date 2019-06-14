deb2ac10eb524e3311513024b36230b8a2629c01
TKOFFICE\huxw <huxw@mail.jj.cn>
2019-06-14 12:43:30 +0800

    添加测试的时间显示

--- docs/lottery.js
+++ docs/lottery.js
@@ -1,22 +1,22 @@
 function browser() {
-  var ua = navigator.userAgent,
-    isWindowsPhone = /(?:Windows Phone)/.test(ua),
-    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
-    isAndroid = /(?:Android)/.test(ua),
-    isFireFox = /(?:Firefox)/.test(ua),
-    isChrome = /(?:Chrome|CriOS)/.test(ua),
-    isTablet =
-      /(?:iPad|PlayBook)/.test(ua) ||
-      (isAndroid && !/(?:Mobile)/.test(ua)) ||
-      (isFireFox && /(?:Tablet)/.test(ua)),
-    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
-    isPc = !isPhone && !isAndroid && !isSymbian;
-  return {
-    isTablet: isTablet,
-    isPhone: isPhone,
-    isAndroid: isAndroid,
-    isPc: isPc
-  };
+    var ua = navigator.userAgent,
+        isWindowsPhone = /(?:Windows Phone)/.test(ua),
+        isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+        isAndroid = /(?:Android)/.test(ua),
+        isFireFox = /(?:Firefox)/.test(ua),
+        isChrome = /(?:Chrome|CriOS)/.test(ua),
+        isTablet =
+            /(?:iPad|PlayBook)/.test(ua) ||
+            (isAndroid && !/(?:Mobile)/.test(ua)) ||
+            (isFireFox && /(?:Tablet)/.test(ua)),
+        isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+        isPc = !isPhone && !isAndroid && !isSymbian;
+    return {
+        isTablet: isTablet,
+        isPhone: isPhone,
+        isAndroid: isAndroid,
+        isPc: isPc
+    };
 }
 
 var PRIZE_COUNT = 6;
@@ -24,37 +24,59 @@ var enabledDraw = true;
 
 var prizesEl = null;
 
+var lStart = document.getElementById("lStart");
+var lEnd = document.getElementById("lEnd");
+var lTiming = document.getElementById("lTiming");
+
+var startT, endT;
+
 // 如下是切换旋转方式逻辑
 var rtype = new URL(location).searchParams.get("rtype");
 if (rtype === "1") {
-  radio1.checked = true;
-  prizesEl = document.querySelector(".draw-prizes");
+    radio1.checked = true;
+    prizesEl = document.querySelector(".draw-prizes");
 } else {
-  prizesEl = document.querySelector(".draw-btn");
+    prizesEl = document.querySelector(".draw-btn");
 }
 typeSelect.addEventListener("change", function(ev) {
-  var val = ev.target.value;
-  location.href = location.href.split("?")[0] + "?rtype=" + val;
+    var val = ev.target.value;
+    location.href = location.href.split("?")[0] + "?rtype=" + val;
 });
 
-var lottery = new Lottery(prizesEl, {
-  pits: PRIZE_COUNT,
-  angles: [60, 60, 60, 60, 60, 60]
-});
+var defaultOptions = {
+    pits: PRIZE_COUNT
+};
+
+var lottery = new Lottery(prizesEl, defaultOptions);
 
 lottery.onEnded = function(ins, index, prizeIndexes) {
-  enabledDraw = true;
+    enabledDraw = true;
+    endT = Date.now();
+    lEnd.innerHTML = endT;
+    lTiming.innerHTML = endT - startT + "ms";
 };
 
 var btnDraw = document.querySelector(".draw-btn");
 var eventName = browser().isPc ? "click" : "touchstart";
-
+var timing = 5000;
 btnDraw.addEventListener(eventName, function() {
-  if (!enabledDraw) {
-    return;
-  }
-  // 设置奖项
-  const indexes = [0, 2, 4];
-  lottery.setPrize(indexes);
-  lottery.start();
+    if (!enabledDraw) {
+        return;
+    }
+    // 设置奖项
+    const indexes = [0, 2, 4];
+    lottery.setPrize(indexes);
+    startT = Date.now();
+    lStart.innerHTML = startT;
+    lEnd.innerHTML = "";
+    lTiming.innerHTML = "";
+    lottery.start({
+        ...defaultOptions,
+        timing
+    });
+});
+
+document.getElementById("timing").addEventListener("change", function(ev) {
+    var val = ev.target.value;
+    timing = +val;
 });
